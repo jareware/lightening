0c9fc4e06959d2b46683140311f572049641b51c
Jarno Rantanen <jarno@jrw.fi>
2018-10-13 13:56:32 +0300

    Add trivial WebSocket server.

---
+++ src/lightening/server/index.ts
@@ -0,0 +1,10 @@
+import WebSocket from 'ws';
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('received: %s', message);
+  });
+  ws.send('something');
+});
