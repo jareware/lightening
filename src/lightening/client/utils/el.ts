fa34cc20c2d06f475886c4da750556e4fd81b51d
Jarno Rantanen <jarno@jrw.fi>
2018-10-20 10:09:52 +0300

    Add basic types for el-js.

--- src/lightening/client/utils/el.ts
+++ src/lightening/client/utils/el.ts
@@ -1,3 +1,22 @@
+type ScalarArg = string | number | boolean;
+type FalsyArg = null | undefined | false;
+type ElementProps = { [attribute: string]: string };
+type VarArg = ElementProps | ScalarArg | HTMLElement | FalsyArg | (HTMLElement | FalsyArg)[];
+
+// Convenience method 1: Create a text node
+export function el(tagName: '', textNodeContent: ScalarArg): Text;
+
+// Convenience method 2: Create a comment node
+export function el(tagName: '<!', commentNodeContent: ScalarArg): Comment;
+
+// Convenience method 3: Adopt an existing node
+export function el<N extends HTMLElement>(existingNode: N, ...args: VarArg[]): N;
+
+// Default call signature: a named HTML tag type, followed up with 0 or more content
+export function el<K extends keyof HTMLElementTagNameMap>(tagName: K, ...args: VarArg[]): HTMLElementTagNameMap[K];
+
+// Actual implementation, directly ported from ES5-JS
+// @see https://github.com/jareware/el-js
 export function el(...args: any[]) {
   var name = args[0];
   function isNode(n: any) {
