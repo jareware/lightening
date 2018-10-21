25970ffdc4aca84b5ffb2f494f62c112ee05da5d
Jarno Rantanen <jarno@jrw.fi>
2018-10-21 12:01:07 +0300

    Add a more fleshed out deploy script.

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-# TODO
+SSH="ssh pi@lightening.local"
+WC="lightening"
+TAG="lightening"
+CONTAINER="lightening"
+
+(
+  set -ex
+  $SSH "cd $WC && git pull"
+  $SSH "cd $WC && docker build -t $TAG ."
+  $SSH "docker stop $CONTAINER"
+  $SSH "docker rm $CONTAINER"
+  $SSH "source $WC/.env && docker run --name $CONTAINER -d -p 80:8080 -p 8081:8081 --env LIGHTENING_TRADFRI_HOSTNAME --env LIGHTENING_TRADFRI_IDENTITY --env LIGHTENING_TRADFRI_PSK $TAG"
+)
+
+echo
+echo "✅  Success"
+echo
