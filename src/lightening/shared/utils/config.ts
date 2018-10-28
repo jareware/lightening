2df25a24416ab2fac507fa1b3e9ffda98f52b3e6
Jarno Rantanen <jarno@jrw.fi>
2018-10-28 13:32:27 +0200

    Fix LIGHTENING_WEB_ROOT default value.

--- src/lightening/shared/utils/config.ts
+++ src/lightening/shared/utils/config.ts
@@ -8,7 +8,7 @@ export function readConfigFromEnv() {
     LIGHTENING_TRADFRI_IDENTITY: process.env.LIGHTENING_TRADFRI_IDENTITY || '',
     LIGHTENING_TRADFRI_PSK: process.env.LIGHTENING_TRADFRI_PSK || '',
     LIGHTENING_WEB_PORT: 8080,
-    LIGHTENING_WEB_ROOT: normalize(__dirname + '/../client/'),
+    LIGHTENING_WEB_ROOT: normalize(__dirname + '/../../client/'),
     LIGHTENING_WEBSOCKET_PORT: 8081,
   };
   if (!config.LIGHTENING_TRADFRI_HOSTNAME || !config.LIGHTENING_TRADFRI_IDENTITY || !config.LIGHTENING_TRADFRI_PSK)
