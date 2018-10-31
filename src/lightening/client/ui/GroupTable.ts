25f77be9b03bbe6bb8a8c6ae20ab145467c6e278
Jarno Rantanen <jarno@jrw.fi>
2018-10-31 17:29:41 +0200

    Ensure we can report on mixed on/off state in GroupTable.

--- src/lightening/client/ui/GroupTable.ts
+++ src/lightening/client/ui/GroupTable.ts
@@ -4,6 +4,7 @@ import { WebSocketClient } from 'lightening/client/utils/ws';
 import { setLights } from 'lightening/client/actions/lights';
 import { ServerState } from 'lightening/shared/model/state';
 import { is } from 'lightening/shared/model/utils';
+import { Group } from 'lightening/shared/model/tradfri';
 
 export default (state: ServerState, ws: WebSocketClient) =>
   table(
@@ -26,10 +27,25 @@ export default (state: ServerState, ws: WebSocketClient) =>
             td(group.id),
             td(group.name),
             td(group.devices.length),
-            td(group.on ? 'On' : ''),
+            td(getOnOffState(group, state)),
             td(button('On', { click: () => setLights(ws, group, true) })),
             td(button('Off', { click: () => setLights(ws, group, false) })),
           ),
         ),
     ),
   );
+
+function getOnOffState(group: Group, state: ServerState) {
+  const lightsOn = group.devices
+    .map(id => state.objects[id])
+    .filter(is.Light)
+    .map(light => light.on);
+  const isTrue = (x: boolean) => x === true;
+  const isFalse = (x: boolean) => x === false;
+  if (lightsOn.some(isTrue)) {
+    if (lightsOn.some(isFalse)) return 'mixed';
+    return 'on';
+  } else {
+    return 'off';
+  }
+}
