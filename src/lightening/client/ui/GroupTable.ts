edd992ca1a4a31252ad686b25e925a353c57478c
Jarno Rantanen <jarno@jrw.fi>
2018-11-03 00:24:44 +0200

    Use the new-style is() helper.

--- src/lightening/client/ui/GroupTable.ts
+++ src/lightening/client/ui/GroupTable.ts
@@ -4,14 +4,14 @@ import { WebSocketClient } from 'lightening/client/utils/ws';
 import { setLights } from 'lightening/client/actions/lights';
 import { ServerState } from 'lightening/shared/model/state';
 import { is } from 'lightening/shared/model/utils';
-import { Group, Outlet, Light } from 'lightening/shared/model/tradfri';
+import { Group } from 'lightening/shared/model/tradfri';
 
 export default (state: ServerState, ws: WebSocketClient) =>
   table(
     { class: 'lightening-GroupTable' },
     tbody(
       values(state.devices)
-        .filter(is.Group)
+        .filter(is('Group'))
         .map(group =>
           tr(
             { class: `lightening-GroupTable-${getOnOffState(group, state)}` },
@@ -26,7 +26,7 @@ export default (state: ServerState, ws: WebSocketClient) =>
 function getOnOffState(group: Group, state: ServerState) {
   const lightsOn = group.devices
     .map(id => state.devices[id])
-    .filter((x): x is Light | Outlet => is.Light(x) || is.Outlet(x))
+    .filter(is('Light', 'Outlet'))
     .map(device => device.on);
   const isTrue = (x: boolean) => x === true;
   const isFalse = (x: boolean) => x === false;
