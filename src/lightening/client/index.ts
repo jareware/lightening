888110d45a6b3f56366d1a5746bbb26a949fab2f
Jarno Rantanen <jarno@jrw.fi>
2018-12-02 13:15:14 +0200

    Use LighteningUi component for rendering the root.

--- src/lightening/client/index.ts
+++ src/lightening/client/index.ts
@@ -1,8 +1,8 @@
-import { el, pre } from 'lightening/client/utils/el';
+import { el } from 'lightening/client/utils/el';
 import { createWsClient } from 'lightening/client/utils/ws';
 import { createConsoleLogger } from 'lightening/shared/utils/logging';
-import GroupTable from 'lightening/client/ui/GroupTable';
 import { GlobalState } from 'lightening/shared/model/state';
+import LighteningUi from 'lightening/client/ui/LighteningUi';
 
 const log = createConsoleLogger();
 const wsUrl = location.protocol.replace(/^http/, 'ws') + '//' + location.hostname + ':' + 8081;
@@ -11,11 +11,7 @@ const ws = createWsClient(wsUrl, render, log);
 function render(state: GlobalState) {
   const then = performance.now();
   document.body.innerHTML = '';
-  el(
-    document.body,
-    !state.clientState.webSocketConnected && pre('Connecting...'),
-    state.serverState && GroupTable(state.serverState, ws),
-  );
+  el(document.body, LighteningUi(state, ws));
   const now = performance.now();
   log.debug(`Rendering took ${(now - then).toFixed(1)} ms`);
 }
