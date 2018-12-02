61c04ae91bf4b080f1ce71df51b1309d14de18cd
Jarno Rantanen <jarno@jrw.fi>
2018-12-02 13:34:35 +0200

    Fix UI scaling.

--- src/lightening/client/ui/LighteningUi.ts
+++ src/lightening/client/ui/LighteningUi.ts
@@ -3,9 +3,16 @@ import { GlobalState } from 'lightening/shared/model/state';
 import { WebSocketClient } from 'lightening/client/utils/ws';
 
 export default (state: GlobalState, _ws: WebSocketClient) => {
+  const floorplan = object({
+    type: 'image/svg+xml',
+    data: 'floorplan.svg',
+    load: () => {
+      console.log('SVG loaded', floorplan);
+    },
+  });
   return div(
     { class: 'lightening-LighteningUi' },
     !state.clientState.webSocketConnected && pre('Connecting...'),
-    object({ type: 'image/svg+xml', data: 'floorplan.svg' }),
+    floorplan,
   );
 };
