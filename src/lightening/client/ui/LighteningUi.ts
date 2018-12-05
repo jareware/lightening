071f52370f208730f4b6b0e38b2000ffa1468bba
Jarno Rantanen <jarno@jrw.fi>
2018-12-06 00:08:28 +0200

    Add basic rendering of floor plan.

--- src/lightening/client/ui/LighteningUi.ts
+++ src/lightening/client/ui/LighteningUi.ts
@@ -1,18 +1,31 @@
-import { div, pre, object } from 'lightening/client/utils/el';
+import { div, pre } from 'lightening/client/utils/el';
 import { GlobalState } from 'lightening/shared/model/state';
 import { WebSocketClient } from 'lightening/client/utils/ws';
+import { setLights } from 'lightening/client/actions/lights';
+import { is } from 'lightening/shared/model/utils';
 
-export default (state: GlobalState, _ws: WebSocketClient) => {
-  const floorplan = object({
-    type: 'image/svg+xml',
-    data: 'floorplan.svg',
-    load: () => {
-      console.log('SVG loaded', floorplan);
-    },
-  });
+export default (state: GlobalState, ws: WebSocketClient) => {
   return div(
     { class: 'lightening-LighteningUi' },
     !state.clientState.webSocketConnected && pre('Connecting...'),
-    floorplan,
+    renderFloorPlan(),
   );
+
+  function renderFloorPlan() {
+    if (!state.clientState.floorPlanSvg) return pre('Loading floor plan...');
+    const fp = div({
+      click: (event: any) => {
+        if (!state.serverState) return;
+        const target: HTMLElement | SVGElement | null = event.target;
+        if (!target) return;
+        const id = target.getAttribute('lightening:id');
+        if (!id) return;
+        const group = state.serverState.devices[id];
+        if (!is('Group')(group)) return;
+        setLights(ws, group, 'toggle');
+      },
+    });
+    fp.innerHTML = state.clientState.floorPlanSvg;
+    return fp;
+  }
 };
