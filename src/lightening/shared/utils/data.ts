667a87d8a6fdd8289c1438c0ff2615371726dd3a
Jarno Rantanen <jarno@jrw.fi>
2021-10-02 21:06:33 +0300

    Re-init project.

--- src/lightening/shared/utils/data.ts
+++
@@ -1,7 +0,0 @@
-import { Device } from 'lightening/shared/model/tradfri';
-
-// Important: This isn't applicable to arbitrary objects, only known model types.
-// @see https://github.com/Microsoft/TypeScript/pull/12253#issuecomment-263132208
-export function values<T extends Device>(object: { [key: string]: T }): T[] {
-  return Object.keys(object).map(key => object[key]);
-}
