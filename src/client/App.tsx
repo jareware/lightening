eb1f33c123d2e752f940be253c5bf15d6a0bf5d0
Jarno Rantanen <jarno@jrw.fi>
2022-09-13 21:01:24 +0300

    Add connection retrying to UI.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,47 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react'
 import 'src/client/App.css'
-import { PORT } from 'src/shared/config'
 import { LightGroups } from 'src/server/state'
+import { PORT } from 'src/shared/config'
+
+const url =
+  process.env.NODE_ENV === 'production'
+    ? `ws://${location.hostname}:${location.port}/` // in prod, connect to whatever port the UI was served from
+    : `ws://${location.hostname}:${PORT}/` // in development, we can't connect through the CRA proxy, so connect directly to server process
 
 function App() {
-  const [state, setState] = useState<LightGroups | undefined>()
-  const socket = useRef<WebSocket | undefined>()
-  useEffect(() => {
-    socket.current = new WebSocket(
-      process.env.NODE_ENV === 'production'
-        ? `ws://${location.hostname}:${location.port}/` // in prod, connect to whatever port the UI was served from
-        : `ws://${location.hostname}:${PORT}/`, // in development, we can't connect through the CRA proxy, so connect directly to server process
-    )
-    socket.current.onopen = e => {
-      // socket.current.send('My name is John')
-    }
+  const { connected, state, send } = useServerConnection()
 
-    socket.current.onmessage = function (event) {
-      console.log(`[message] Data received from server: ${event.data}`)
-      setState(JSON.parse(event.data))
-    }
-
-    socket.current.onclose = function (event) {
-      if (event.wasClean) {
-        console.log(
-          `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`,
-        )
-      } else {
-        // e.g. server process killed or network down
-        // event.code is usually 1006 in this case
-        console.log('[close] Connection died')
-      }
-    }
-
-    socket.current.onerror = function (error: any) {
-      console.log(`[error] ${error.message}`)
-    }
-
-    return () => {
-      console.log('Closing the socket because unmounting')
-      socket.current?.close()
-    }
-  }, [])
+  if (!connected) return <pre>NOT CONNECTED</pre>
 
   return (
     <>
@@ -66,7 +36,7 @@ function App() {
             <button
               id={group.friendlyName}
               onClick={() =>
-                socket.current?.send(
+                send(
                   JSON.stringify({
                     device: group.friendlyName,
                     brightness: isOff ? 254 : 0,
@@ -84,3 +54,81 @@ function App() {
 }
 
 export default App
+
+function useServerConnection() {
+  const socket = useRef<
+    ReturnType<typeof createRetryingWebSocket> | undefined
+  >()
+  const [connected, setConnected] = useState(false)
+  const [state, setState] = useState<LightGroups | undefined>()
+
+  useEffect(() => {
+    socket.current = createRetryingWebSocket(
+      url,
+      data => {
+        console.log('Received data from server:', data)
+        setState(JSON.parse(data))
+      },
+      setConnected,
+    )
+    return socket.current.dispose
+  }, [])
+
+  return {
+    connected,
+    state,
+    send: (data: string) => socket.current?.send(data),
+  }
+}
+
+function createWebSocket(
+  url: string,
+  onMessage: (data: string) => void,
+  onStatusChange: (connected: boolean) => void,
+) {
+  console.log('WebSocket connecting')
+  const socket = new WebSocket(url)
+  let disposed = false
+
+  socket.addEventListener('message', e => !disposed && onMessage(e.data))
+  socket.addEventListener('open', () => !disposed && onStatusChange(true))
+  socket.addEventListener('close', () => !disposed && onStatusChange(false))
+  socket.addEventListener('error', () => !disposed && onStatusChange(false))
+
+  return {
+    send: (data: string) => socket.send(data),
+    dispose: () => {
+      console.log('WebSocket disposing')
+      disposed = true
+      socket.close()
+    },
+  }
+}
+
+function createRetryingWebSocket(
+  url: string,
+  onMessage: (data: string) => void,
+  onStatusChange: (connected: boolean) => void,
+) {
+  let socket = createWebSocket(url, onMessage, _onStatusChange)
+  let timeout: NodeJS.Timeout | undefined
+
+  return {
+    send: (data: string) => socket.send(data),
+    dispose: () => {
+      timeout && clearTimeout(timeout)
+      socket.dispose()
+    },
+  }
+
+  function _onStatusChange(connected: boolean) {
+    onStatusChange(connected)
+    if (!connected) {
+      socket.dispose()
+      setTimeout(
+        () => (socket = createWebSocket(url, onMessage, _onStatusChange)),
+        5000,
+      )
+    }
+  }
+}
