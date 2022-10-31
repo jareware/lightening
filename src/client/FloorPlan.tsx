153d19462859fcf5706aa5a05cb5f528ef2889b6
Jarno Rantanen <jarno@jrw.fi>
2022-10-31 08:50:50 +0200

    Tweak wall/door positions.

--- src/client/FloorPlan.tsx
+++ src/client/FloorPlan.tsx
@@ -13,59 +13,59 @@ export function FloorPlan() {
         style={{ maxHeight: '80vh' }}
       >
         <Wall
+          note="parveke"
+          path="M 726 348 l 192 -34 l 48 246 l -192 34"
+          light
+        />
+        <Wall
           note="ulkoseinät"
           path="M 32 27 v 818 h 790 L 662 27 Z"
-          dash="1086 55 507 94 26 50 67 150 5 54 147 160 181 50 34 107 85 93 18 49"
+          dash="0 1086 70 492 94 26 50 67 139 15 55 147 160 181 50 34 107 85 93 18 49 0"
         />
         <Wall
           note="makkari"
-          path="M 274 27 v 308 h -241"
-          dash="237 55 139 48"
+          path="M 274 29 v 306 h -240"
+          dash="0 230 55 148 55 0"
         />
         <Wall
           note="käytävän vasen seinä"
-          path="M 230 335 v 250 h 44 v 260"
-          dash="313 54 83 54"
+          path="M 230 337 v 248 h 44 v 257"
+          dash="0 313 55 90 55 0"
         />
         <Wall
           note="vaatehuoneen vasen kotelo"
-          path="M 32 456 h 46 v 26 h -46 Z"
+          path="M 32 456 h 37 v 26 h -37 Z"
           filled
         />
         <Wall
           note="vaatehuoneen oikea kotelo"
-          path="M 158 482 v -27 h 72 v 27 Z"
+          path="M 169 482 v -27 h 55 v 27 Z"
           filled
         />
         <Wall
           note="kylppärin ja vaatehuoneen väliseinä"
           path="M 32 483 h 198"
-          dash="66 48 100"
+          dash="0 59 55 100 0"
         />
-        <Wall note="saunan väliseinä" path="M 32 720 h 168" dash="99 47" />
-        <Wall note="pikkuvessan seinä" path="M 273 685 h -73 v 160" />
+        <Wall note="saunan väliseinä" path="M 34 720 h 162" dash="0 90 55 0" />
+        <Wall note="pikkuvessan seinä" path="M 271 685 h -71 v 158" />
         <Wall note="keittiön oikea seinä" path="M 376 336 h 51 v 249 h -58" />
-        <Wall note="siivouskaapin ovi" path="M 510 583 v 60" light />
+        <Wall note="siivouskaapin ovi" path="M 510 587 v 50" light />
         <Wall
           note="Emman huoneen seinä 1"
-          path="M 773 594 l -263 46 v 93 h -111"
-          dash="288 54"
+          path="M 772 594 l -262 46 v 93 h -111"
+          dash="0 285 55 0"
         />
-        <Wall note="Emman huoneen seinä 1" path="M 460 734 v 111" />
+        <Wall note="Emman huoneen seinä 1" path="M 460 735 v 109" />
         <Wall
           note="siivouskaapin ulkoseinä"
-          path="M 425 585 h 185 l 6 36"
-          dash="17 55 170"
-        />
-        <Wall
-          note="parveke"
-          path="M 725 348 l 193 -34 l 48 248 l -194 32"
-          light
+          path="M 429 585 h 181 l 6 36"
+          dash="0 18 55 170 0"
         />
         <Wall
           note="olohuoneen ja työhuoneen väliseinä"
           path="M 430 365 l 290 -32"
-          dash="17 55 1000"
+          dash="0 17 55 1000 0"
         />
       </svg>
     </div>
@@ -89,12 +89,13 @@ function Wall(props: {
   }
   return (
     <>
-      <path
-        d={props.path}
-        style={{ ...style, stroke: props.dash ? 'lightgray' : 'black' }}
-      />
+      <path d={props.path} style={style} />
       {props.dash && (
-        <path d={props.path} strokeDasharray={props.dash} style={style} />
+        <path
+          d={props.path}
+          style={{ ...style, stroke: 'lightgray' }}
+          strokeDasharray={props.dash}
+        />
       )}
     </>
   )
